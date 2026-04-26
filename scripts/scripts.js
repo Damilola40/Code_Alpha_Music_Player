7b6f82f586b83eab72253e2da5efbdf0606d0028
Muhammad Misbahudeen <muhammadmisbahudeen4@gmail.com>
2026-04-26 15:34:53 +0100

    Project 80% completed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,20 @@
+// Toggle Dark Mode
+const themeToggle = document.querySelector(".theme-toggle");
+const themeIcon = document.querySelector(".theme-toggle i");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    
+    // swap the icon
+    if (document.body.classList.contains("dark-mode")) {
+        themeIcon.classList.remove("fa-moon");
+        themeIcon.classList.add("fa-sun");
+    } else {
+        themeIcon.classList.remove("fa-sun");
+        themeIcon.classList.add("fa-moon");
+    }
+})
+
 // Displaying the playlist
 const playlistBtn = document.querySelector(".fa-bars").parentElement;
 const playlist = document.getElementById("list");
@@ -22,15 +39,15 @@ backBtn.addEventListener("click", () => {
 
 // Choosing a Song
 const cover_photo = document.getElementById("cover-photo");
-const audio = document.getElementsByTagName("audio");
+const audio = document.querySelector("audio");
 const song_title = document.getElementById("song-title");
 const song_artist = document.getElementById("artist");
-
+let currentSongIndex = 0
 const songs = [
     {
         title: "Burdah",
         artist: "Mesut Kurtis",
-        src: "music/Burdah(128k).mp3",
+        src: "music/Burdah.mp3",
         cover: "images/burdah.jfif"
     },
     {
@@ -38,27 +55,160 @@ const songs = [
         artist: "Abdou Salam",
         src: "music/The tune of hope.m4a",
         cover: "images/hope.jfif"
+    },
+    {
+        title: "Kamin",
+        artist: "Emin ft. JONY",
+        src: "music/KAMIN.mp3",
+        cover: "images/kamin.jfif"
     }
 ]
 
+const loadSong = (index) => {
+    const song = songs[index];
+    cover_photo.src = song.cover;
+    audio.src = song.src;
+    song_title.innerText = song.title;
+    song_artist.innerText = song.artist;
+}
+loadSong(currentSongIndex);
 
-// Toggle Dark Mode
-const themeToggle = document.querySelector(".theme-toggle");
-const themeIcon = document.querySelector(".theme-toggle i");
 
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    
-    // swap the icon
-    if (document.body.classList.contains("dark-mode")) {
-        themeIcon.classList.remove("fa-moon");
-        themeIcon.classList.add("fa-sun");
+// Progress Bar
+const progressBar = document.getElementById("progress");
+const songDuration = document.getElementById("duration")
+const current_time = document.getElementById("current-time");
+audio.onloadedmetadata = function() {
+    progressBar.max = audio.duration; 
+    progressBar.value = 0;
+}
+audio.ontimeupdate = function() {
+    progressBar.value = song.currentTime; 
+    current_time.innerText = formatTime(song.currentTime);
+    songDuration.innerText = formatTime(song.duration);
+}
+const formatTime = (seconds) => {
+    const mins = Math.floor(seconds / 60); // how many full minutes
+    const secs = Math.floor(seconds % 60); // remaining seconds
+    return `${mins}:${secs < 10 ? "0" + secs : secs}`; // combine as "2:05"
+}
+        
+
+// Playing a song
+const playBtn = document.querySelector(".play-pause-btns");
+const playBtnIcon = document.querySelector(".play-pause-btns i");
+let isPlaying = false;
+
+const togglePlay = () => {
+    if (isPlaying) {
+        audio.pause();
+        playBtnIcon.classList.remove("fa-pause");
+        playBtnIcon.classList.add("fa-play");
+        isPlaying = false;
     } else {
-        themeIcon.classList.remove("fa-sun");
-        themeIcon.classList.add("fa-moon");
+        isPlaying = true;
+        audio.play()
+        console.log("playing successfully"); // does this show?
+        playBtnIcon.classList.remove("fa-play");
+        playBtnIcon.classList.add("fa-pause");
+        // Seeking
+        progressBar.addEventListener("input", () => {
+            audio.play();
+            audio.currentTime = progressBar.value;
+            playBtnIcon.classList.remove("fa-play");
+            playBtnIcon.classList.add("fa-pause");
+        })
     }
+}
+playBtn.addEventListener("click", togglePlay);
+    
+
+// Next and Previous Buttons
+const forward = document.querySelector(".forward");
+const backward = document.querySelector(".backward");
+
+function forwardBtn () {
+    currentSongIndex += 1;
+    if (currentSongIndex >= songs.length) {
+        console.log("forward worked")
+        currentSongIndex = 0;
+    }
+    loadSong(currentSongIndex);
+    if (isPlaying) {
+        audio.play();
+    }
+}
+forward.addEventListener("click", forwardBtn)
+
+const backwardBtn = () => {
+    currentSongIndex -= 1;
+    if (currentSongIndex < 0) {
+        console.log("backward worked")
+        currentSongIndex = songs.length - 1;
+    }
+    loadSong(currentSongIndex);
+    if (isPlaying) {
+        audio.play()
+    }
+}
+backward.addEventListener("click", backwardBtn)
+
+// Shuffle Button
+let isShuffle = false;
+const shuffleBtn = document.querySelector(".shuffle");
+shuffleBtn.addEventListener("click", () => {
+    console.log("shuffle clicked")
+    isShuffle = !isShuffle;
+    shuffleBtn.classList.toggle("active");
+})
+
+// Repeat Button
+let isRepeat = false;
+const repeatBtn = document.querySelector(".repeat")
+repeatBtn.addEventListener("click", () => {
+    console.log("repeat clicked");
+    isRepeat = !isRepeat;
+    repeatBtn.classList.toggle("active");
 })
 
 
+audio.addEventListener("ended", () => {
+    progressBar.value = 0;
+    current_time.innerText = "0:00"; // set timer to default
+    playBtnIcon.classList.remove("fa-pause");
+    playBtnIcon.classList.add("fa-play"); 
+    if (isShuffle) {
+        console.log("shuffle activated");
+        currentSongIndex = Math.floor(Math.random() * songs.length);
+        loadSong(currentSongIndex);
+        audio.play();
+        playBtnIcon.classList.remove("fa-play");
+        playBtnIcon.classList.add("fa-pause");
+    } // shuffle playlist
+    if (isRepeat) {
+        console.log("repeat activated");
+        audio.currentTime = 0;
+        audio.play();
+        playBtnIcon.classList.remove("fa-play");
+        playBtnIcon.classList.add("fa-pause");
+    } else {
+        forwardBtn
+    } // repeat song
+})
+
+// Volume Button
+const volumeBtn = document.getElementById("volume");
+const volumeBar = document.querySelector(".volume-bar");
+
+volumeBtn.addEventListener("click", () => {
+    if (volumeBar.style.display === "block") {
+        volumeBar.style.display = "none";
+    } else {
+        volumeBar.style.display = "block";
+    }
+})
+volumeBar.addEventListener("input", () => {
+    audio.volume = volumeBar.value;
+})
 
 
